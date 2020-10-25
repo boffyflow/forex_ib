42a492138f00af9fa584ff29dca11e11db60e064
boffyflow <boffyflow@gmail.com>
2020-10-25 12:26:14 -0700

    safety

--- ib_bot.py
+++ ib_bot.py
@@ -8,44 +8,52 @@ from ibapi import wrapper
 from ibapi import utils
 from ibapi.client import EClient
 from ibapi.wrapper import EWrapper
-from ibapi.contract import Contract
 from ibapi.utils import iswrapper
 from candle import Candle, CandleGroup
 
 import config
+from contracts import ForexContract
 
 class IBapi( EWrapper, EClient):
 
-    def __init__( self):
+    def __init__( self, pairs):
         EClient.__init__( self, self)
+        self.pairs = pairs
         self.data = []
         self.lastclose = 0.0
         self.datasize = 0
-
+        
     def historicalData( self, reqId, bar):
-        self.data.insert(0, [bar.date, bar.open, bar.high, bar.low, bar.close, bar.volume])
-        self.datasize = len( self.data)
-        print( datetime.fromtimestamp( int(self.data[0][0])).strftime( '%b %d %Y %H:%M:%S'))
+        for cp in self.pairs:
+            if reqId == cp.requestId():
+                self.data.insert(0, [cp.string(), bar.date, bar.open, bar.high, bar.low, bar.close, bar.volume])
+                self.datasize[cp.requestId()] = len( self.data[cp.requestId()])
+                print( cp.string(), ':' , datetime.fromtimestamp( int(self.data[cp.requestId()][0][0])).strftime( '%b %d %Y %H:%M:%S'))
 
     def historicalDataUpdate( self, reqId, bar):
-        if( bar.close != self.lastclose):
-            self.lastclose = bar.close
-            if bar.date > self.data[0][0]:
-                self.data.insert(0, [bar.date, bar.open, bar.high, bar.low, bar.close])
-                self.isdirty = True
-            elif bar.date == self.data[0][0]:
-                self.data[0] = [bar.date, bar.open, bar.high, bar.low, bar.close, bar.volume]
-            ts = datetime.fromtimestamp( int( bar.date))
-            print('closing price price:', bar.close, ', date:', ts.strftime( '%b %d %Y %H:%M:%S'))
+        for cp in self.pairs:
+            if( bar.close != self.lastclose[cp.requestId()]):
+                self.lastclose[cp.requestId()] = bar.close
+                if bar.date > self.data[cp.requestId()][0][0]:
+                    self.data[cp.requestId()].insert(0, [bar.date, bar.open, bar.high, bar.low, bar.close])
+                    self.isdirty[cp.requestId()] = True
+                elif bar.date == self.data[cp.requestId()][0][0]:
+                    self.data[cp.requestId()][0] = [bar.date, bar.open, bar.high, bar.low, bar.close, bar.volume]
+                ts = datetime.fromtimestamp( int( bar.date))
+                print(cp.string(), ': closing rice:', bar.close, ', date:', ts.strftime( '%b %d %Y %H:%M:%S'))
 
-    def isDirty(self):
-        if len( self.data) != self.datasize:
-            self.datasize = len( self.data)
+    def isDirty(self, fc):
+        if len( self.data[fc.requestId()]) != self.datasize[fc.requestId()]:
+            self.datasize[fc.requestId()] = len( self.data[fc.requestId()])
             return True
         else:
             return False
 
-app = IBapi()
+
+pairs = []
+for cp in config.CURRENCYPAIRS:
+    pairs.append( ForexContract( cp))
+app = IBapi( pairs)
 
 def run_loop():
 
@@ -63,33 +71,28 @@ def main():
 
     app.connect( '127.0.0.1', 7496, 123)
     
-    api_thread =threading.Thread( target=run_loop, daemon=True)
+    api_thread = threading.Thread( target=run_loop, daemon=True)
     api_thread.start()
 
     time.sleep( 1)
 
-    usdcad_contract = Contract()
-    usdcad_contract.symbol = 'USD'
-    usdcad_contract.secType = 'CASH'
-    usdcad_contract.exchange = 'IDEALPRO'
-    usdcad_contract.currency = 'CAD'
-
-    requestID = 100
-    app.reqHistoricalData( requestID, usdcad_contract, '', '10 D', '1 hour', 'BID', 0, 2, True, [])
+    for fc in pairs:
+        app.reqHistoricalData( fc.requestid(), fc.contract(), '', '10 D', '1 hour', 'BID', 0, 2, True, [])
 
     try:
         while True:
-            time.sleep( 3)
-            if app.isDirty():
-                candles = []
-                ts = datetime.fromtimestamp( int( app.data[1][0]))
-                print('checking for big shadow for candle @', ts.strftime( '%b %d %Y %H:%M:%S'))
-                for i in range( 1, config.BS_NUMCANDLES + 1):
-                    candles.append( Candle( app.data[i][1], app.data[i][2], app.data[i][3], app.data[i][4]))
-                cg = CandleGroup( candles)
-                if cg.bigShadow( maxBodyRatio=config.BS_BODYRATIO, wickPercent=config.BS_WICKPERCENTAGE):
-                    print('big shadow found for candle @', ts.strftime( '%b %d %Y %H:%M:%S'))
-                    sendtext( ( 'found big shadow on USDCAD 1H @', ts.strftime( '%b %d %Y %H:%M:%S')))
+            time.sleep( 1)
+            for cp in pairs:
+                if app.isDirty(cp):
+                    candles = []
+                    ts = datetime.fromtimestamp( int( app.data[cp.requestId()][1][0]))
+                    print( cp.string(), ': checking for big shadow for candle @', ts.strftime( '%b %d %Y %H:%M:%S'))
+                    for i in range( 1, config.BS_NUMCANDLES + 1):
+                        candles.append( Candle( app.data[cp.requestId()][i][1], app.data[cp.requestId()][i][2], app.data[cp.requestId()][i][3], app.data[cp.requestId()][i][4]))
+                    cg = CandleGroup( candles)
+                    if cg.bigShadow( maxBodyRatio=config.BS_BODYRATIO, wickPercent=config.BS_WICKPERCENTAGE):
+                        print( cp.string(), ': big shadow found for candle @', ts.strftime( '%b %d %Y %H:%M:%S'))
+                        sendtext( ( cp.string(), ': found big shadow on USDCAD 1H @', ts.strftime( '%b %d %Y %H:%M:%S')))
 
     except KeyboardInterrupt:
         pass
